1a01912617b47bcd9defabd3eaf06c89cc435135
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-01-12 11:52:24 -0600

    Agregado de propiedades

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ console.log(mexico.comida);
 
 // BRACKET NOTATION - NOTACION POR CORCHETES
 console.log(mexico["comida"]);
+
+const olympicRecords = {
+    athletics100Men: "Justin Gatlin",
+    athleticsLongJumpMen: "Mike Powel",
+    swimming200Men: "Michael Phelps",
+    "swimming 200 Women": "Ariarne Titmus" // Si quisieramos propiedades con espacios. Sin embargo, usando esto no se puede usar dot notation. Acceso solo por dot notation.
+}
+
+console.log(olympicRecords.athletics100Men);
+console.log(olympicRecords["athletics100Men"]);
+
+console.log(olympicRecords["swimming 200 Women"]);
+
+// B. AGREGAR PROPIEDADES
+
+const refresco = {
+    nombre: "Coca cola",
+    color: "Oscuro",
+    precio: 12,
+    tamanos: [600,1000]
+}
+
+refresco.envase = "Plastico"
+
+console.log("Objeto de refresco:", refresco)
+
+refresco["competidor"] = "Pepsi"
+
+console.log (refresco)
